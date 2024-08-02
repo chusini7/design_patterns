c68cfb5ef0e7e01b0511befa60b4676ad71b3fb0
libiao <libiao@efreight.cn>
2024-08-02 15:04:14 +0800

    解释器模式

--- src/main/java/demo/pattern/interpreter/Test.java
+++ src/main/java/demo/pattern/interpreter/Test.java
@@ -8,24 +8,16 @@ package demo.pattern.interpreter;
 public class Test {
 
     public static void main(String[] args) {
-        Expression isMale = getMaleExpression();
-        Expression isMarriedWoman = getMarriedWomanExpression();
-
-        System.out.println("John is male? " + isMale.interpret("John"));
-        System.out.println("Julie is a married women? " + isMarriedWoman.interpret("Married Julie"));
-    }
-
-    // 规则：Robert 和 John 是男性
-    public static Expression getMaleExpression() {
+        // 规则：Robert 和 John 是男性
         Expression robert = new TerminalExpression("Robert");
         Expression john = new TerminalExpression("John");
-        return new OrExpression(robert, john);
-    }
+        Expression isMale = new OrExpression(robert, john);
+        System.out.println("John is male? " + isMale.interpret("John"));
 
-    // 规则：Julie 是一个已婚的女性
-    public static Expression getMarriedWomanExpression() {
+        // 规则：Julie 是一个已婚的女性
         Expression julie = new TerminalExpression("Julie");
         Expression married = new TerminalExpression("Married");
-        return new AndExpression(julie, married);
+        Expression isMarriedWoman =  new AndExpression(julie, married);
+        System.out.println("Julie is a married women? " + isMarriedWoman.interpret("Married Julie"));
     }
 }
